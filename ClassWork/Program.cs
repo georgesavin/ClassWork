25f72e33b5753b47872e8e5dd8242a6a9341168d
georgesavin <31357590+georgesavin@users.noreply.github.com>
2017-09-13 14:31:41 -0600

    fibanoci

---
+++ ClassWork/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassWork
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			int MAX = 1000;
+			int x = 1;
+			int y = 1;
+
+			Console.WriteLine(x);
+			Console.WriteLine(y);
+
+			while (x + y < MAX)
+			{
+				int sum = x + y;
+				x = y;
+				y = sum;
+				Console.WriteLine(sum);
+			}
+        }
+    }
+}
+//this makes you do fibanocci sequance'
+// notes for class
